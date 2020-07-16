5421136a141a3d9c4f81bdafb515d9c448ffbe29
alina-kontarero <alina.kontarero@gmail.com>
2020-07-16 19:49:16 +1000

    CRUD Methods

--- app.js
+++ app.js
@@ -24,15 +24,85 @@ app.get('/api/v1/tours', (req, res) => {
     status: 'success',
     results: tours.length,
     data: {
-      tours
+      tours: tours 
+    }
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params)
+
+  const id = req.params.id * 1
+
+  if(!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found'
+    })
+  }
+
+  const tour = tours.find(el => el.id === id)
+  res.status(200).json({
+    status: 'success',
+    // results: tours.length,
+    data: {
+      tour
     }
   });
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Done!');
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({id: newId}, req.body)
+  tours.push(newTour)
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    // 201 = `created`
+    res.status(201).json({
+      status: "success", 
+      data: {
+        tour: newTour
+      }
+    })
+  })
 });
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+  const id = req.params.id * 1
+
+  if(id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found'
+    })
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+  const id = req.params.id * 1
+
+  if(id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found'
+    })
+  }
+
+  // 204 = `no content` 
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+})
 
 const port = 3000;
 
